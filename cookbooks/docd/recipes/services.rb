9f22bc58e4cf01b25a6f113702f63e632a1396fb
Mike Martin <mike@unsupported.me>
2016-02-08 03:48:34 -0600

    Adds runit service

--- cookbooks/docd/recipes/services.rb
+++ cookbooks/docd/recipes/services.rb
@@ -1,4 +1,18 @@
-services = %w[nginx postgresql]
+services = %w[nginx postgresql runsvdir]
+
+file '/etc/init/runsvdir.conf' do
+  content <<EOM
+# for runit - manage /usr/sbin/runsvdir-start
+start on runlevel 2
+start on runlevel 3
+start on runlevel 4
+start on runlevel 5
+stop on shutdown
+respawn
+exec /usr/sbin/runsvdir-start
+EOM
+  user 'root'; group 'root'; mode 644
+end
 
 services.each do |svc|
   service svc do
