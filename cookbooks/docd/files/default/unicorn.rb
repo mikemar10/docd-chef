74da7de4a7fc2f2538340486b8a8f31488ac2347
Mike Martin <mike@unsupported.me>
2016-02-08 23:46:41 -0600

    Adds unicorn configuration and daemon

---
+++ cookbooks/docd/files/default/unicorn.rb
@@ -0,0 +1,4 @@
+listen '/home/docd/docd/shared/tmp/sockets/unicorn.sock'
+pid '/home/docd/docd/shared/tmp/pids/unicorn.pid'
+timeout 60
+worker_processes 4
---
+++ cookbooks/docd/recipes/unicorn.rb
@@ -0,0 +1,28 @@
+cookbook_file '/home/docd/docd/shared/config/unicorn.rb' do
+  source 'unicorn.rb'
+  user 'docd'; group 'docd'; mode '644'
+end
+
+# runit
+directory '/etc/service/docd_unicorn/log' do
+  user 'root'; group 'root'; mode '755'
+  recursive true
+end
+
+file '/etc/service/docd_unicorn/log/run' do
+  content <<EOM
+#!/bin/bash
+set -ex
+exec svlogd -tt /var/log/unicorn
+EOM
+  user 'root'; group 'root'; mode '755'
+end
+
+file '/etc/service/docd_unicorn/run' do
+  content <<EOM
+#!/bin/bash
+set -ex
+exec chpst -u docd:docd -/home/docd/docd/current unicorn -c config/unicorn.rb
+EOM
+  user 'root'; group 'root'; mode '755'
+end
