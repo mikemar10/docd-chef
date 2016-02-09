7a675cd7de8cc37d16f64ede9bbeedfa2b3875ae
Mike Martin <mike@unsupported.me>
2016-02-08 22:22:46 -0600

    Use postgres system user for postgres management

--- cookbooks/docd/recipes/database.rb
+++ cookbooks/docd/recipes/database.rb
@@ -1,12 +1,14 @@
 database_credentials = Chef::EncryptedDataBagItem.load(:docd, :database)
 
 execute 'create docd database user' do
-  command "psql -U postgres -c \"CREATE ROLE docd ENCRYPTED PASSWORD #{database_credentials['password']} CREATEDB LOGIN;\""
-  not_if "psql -U postgres -c \"SELECT * FROM pg_user WHERE usename='docd'\" | grep docd"
+  command "psql -c \"CREATE ROLE docd ENCRYPTED PASSWORD #{database_credentials['password']} CREATEDB LOGIN;\""
+  user 'postgres'
+  not_if "psql -c \"SELECT * FROM pg_user WHERE usename='docd'\" | grep docd"
 end
 
 execute 'create docd database' do
-  command 'createdb -U postgres -O docd docd'
-  not_if "psql -U postgres -c \"SELECT * FROM pg_database WHERE datname='docd'\" | grep docd"
+  command 'createdb -O docd docd'
+  user 'postgres'
+  not_if "psql -c \"SELECT * FROM pg_database WHERE datname='docd'\" | grep docd"
 end
 
