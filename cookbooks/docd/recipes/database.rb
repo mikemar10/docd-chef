88d2eae129a9643ae34e09dbe651180d4d0074a5
Mike Martin <mike@unsupported.me>
2016-02-08 22:24:19 -0600

    Adds missing quotes and semicolons to SQL query

--- cookbooks/docd/recipes/database.rb
+++ cookbooks/docd/recipes/database.rb
@@ -1,14 +1,14 @@
 database_credentials = Chef::EncryptedDataBagItem.load(:docd, :database)
 
 execute 'create docd database user' do
-  command "psql -c \"CREATE ROLE docd ENCRYPTED PASSWORD #{database_credentials['password']} CREATEDB LOGIN;\""
+  command "psql -c \"CREATE ROLE docd ENCRYPTED PASSWORD '#{database_credentials['password']}' CREATEDB LOGIN;\""
   user 'postgres'
-  not_if "psql -c \"SELECT * FROM pg_user WHERE usename='docd'\" | grep docd"
+  not_if "psql -c \"SELECT * FROM pg_user WHERE usename='docd';\" | grep docd"
 end
 
 execute 'create docd database' do
   command 'createdb -O docd docd'
   user 'postgres'
-  not_if "psql -c \"SELECT * FROM pg_database WHERE datname='docd'\" | grep docd"
+  not_if "psql -c \"SELECT * FROM pg_database WHERE datname='docd';\" | grep docd"
 end
 
